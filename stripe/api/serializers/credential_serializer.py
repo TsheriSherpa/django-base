8af9868553d08f091880d649d5e2e5bae5a5afc2
Chiree Sherpa <tsherisherpa@gmail.com>
2022-08-03 14:20:46 +0545

    added khalti app

--- stripe/api/serializers/credential_serializer.py
+++ stripe/api/serializers/credential_serializer.py
@@ -5,3 +5,8 @@ class CredentialSerializer(serializers.Serializer):
     base_url = serializers.CharField(max_length=255)
     secret_key = serializers.CharField(max_length=255)
     publishable_key = serializers.CharField(max_length=255)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['type'] = 'stripe'
+        return data
