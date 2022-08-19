6b44deb9756cbc12be1dcfeb925631feb5aa74ca
Chiree Sherpa <tsherisherpa@gmail.com>
2022-08-19 15:42:58 +0545

    esewa refactor

--- esewa/api/views/esewa_payment_views.py
+++ esewa/api/views/esewa_payment_views.py
@@ -1,11 +1,14 @@
-from app.api.services.app_service import AppService
-from esewa.api.services.esewa_service import EsewaService
 from rest_framework import generics
-from app.api.permissions.authenticated_app import IsAuthenticatedApp
 from rest_framework.throttling import UserRateThrottle
-from utils.api_service import ApiService
+
+from drf_yasg.utils import swagger_auto_schema
 from utils.helpers import get_client_ip
-from rest_framework.response import Response
+
+from app.api.services.app_service import AppService
+from app.api.permissions.authenticated_app import IsAuthenticatedApp
+from esewa.api.serializers.esewa_verify_serializer import EsewaVerifySerializer
+from esewa.api.services.esewa_service import EsewaService
+
 
 class EsewaPaymentView(generics.GenericAPIView):
     """ View for payment payhment
@@ -17,27 +20,30 @@ class EsewaPaymentView(generics.GenericAPIView):
         Response: Whether payment is success or not
     """
 
-    authentication_classes=[IsAuthenticatedApp]
-    throttle_classes=[UserRateThrottle]
+    throttle_classes = [UserRateThrottle]
 
-    service=None
+    service = None
 
     def __init__(self) -> None:
         super().__init__()
-        self.service=EsewaService()
+        self.service = EsewaService()
 
-    def get(self,request):
-        print(request.app)
-        """Get List Of Payment Available For App
+    @swagger_auto_schema(
+        request_body=EsewaVerifySerializer,
+        responses={
+            200: "Redirects to esewa webpage"
+        }
+    )
+    def post(self, request):
+        """Esewa webpayment view
 
-        Args:
-            request (request): django request
+            Args:
+                request (request): django request
 
-        Returns:
-            list:
+            Returns:
+                Redirect: redirects to esewa payment webpage
         """
-
-        log=self.service.create_transaction_log(
+        self.service.create_transaction_log(
             request.app,
             request.GET['credential_type'],
             request.GET['environment'],
@@ -49,5 +55,6 @@ class EsewaPaymentView(generics.GenericAPIView):
             request.GET
         )
 
-        credential=AppService.get_credential(request.app,'esewa',request.GET['credential_type'],request.GET['environment'])
-        return self.service.payment_transaction(request,credential,request.GET)
+        credential = AppService.get_credential(
+            request.app, 'esewa', request.GET['credential_type'], request.GET['environment'])
+        return self.service.payment_transaction(request, credential, request.GET)
