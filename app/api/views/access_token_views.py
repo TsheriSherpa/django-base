6efbab612fe0a1b386fad5fc1ceb165343f8f557
Chiree Sherpa <tsherisherpa@gmail.com>
2022-08-19 12:20:26 +0545

    refactor

--- app/api/views/access_token_views.py
+++ app/api/views/access_token_views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics
-from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
 
 from app.models import App
 from app.api.auth.auth import Auth
 from app.api.serializers.app_access_token_serializer import AppAccessTokenSerializer
+from utils.api_response import ApiResponse
 from utils.helpers import get_error_message
 
 
@@ -32,30 +32,18 @@ class AccessTokenView(generics.GenericAPIView):
 
         serializer = self.get_serializer(data=request.data)
         if not serializer.is_valid():
-            return Response({
-                "status": False,
-                "error": get_error_message(serializer)
-            }, 422)
+            return ApiResponse.send_error(get_error_message(serializer),  400)
 
         auth = Auth(request,  App)
         try:
             app = auth.app_from_refresh_token()
         except Exception:
-            return Response({
-                'status': False,
-                'error': 'Invalid Refresh Token'
-            })
+            return ApiResponse.send_error('Invalid Refresh Token',  400)
 
         if not app:
-            return Response({
-                'status': False,
-                'error': 'Invalid Refresh Token'
-            })
+            return ApiResponse.send_error('Invalid Refresh Token',  400)
 
         token = auth.generate_access_token(
             app, {}, request.data['refresh_token'])
 
-        return Response({
-            'status': True,
-            'data': token
-        }, 200)
+        return ApiResponse.send_success(token)
